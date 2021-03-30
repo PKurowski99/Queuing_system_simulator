7593b0ff34f2119e138c47059967cee0cd128a19
Natalia Kwasek <n.kwasek@stud.elka.pw.edu.pl>
2021-03-30 20:42:45 +0200

    Natalia's commit

---
+++ Pakiet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOPS
+{
+    class Pakiet
+    {
+        public Pakiet(TypRuchu typ) { this.typ = typ; }
+
+        private int idWezla;
+        private double czasPrzyjscia;
+        private double opoznienie = 1;
+        private TypRuchu typ;
+
+        public TypRuchu getTypRuchu() { return typ; }
+        public double getOpoznienie() { return opoznienie; }
+        public void zmienId(int id) { this.idWezla = id; }
+        public void ustawCzasPrzyjscia(double czas) { this.czasPrzyjscia = czas; }
+        public int getId() { return idWezla; }
+        public void obliczOpoznienie(double czasRozpoczeciaObslugi)
+        {
+            opoznienie = czasRozpoczeciaObslugi - czasPrzyjscia;
+        }
+        public void setOpoznienie(double czas)
+        {
+            opoznienie = czas;
+        }
+    }
+}
---
+++ ZrodloRuchu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace MOPS
+{
+    public enum TypRuchu { podkladowy, testowy };
+    class ZrodloRuchu
+    {
+        public ZrodloRuchu()
+        {
+            lambda = 2;
+        }
+
+        public ZrodloRuchu(double lam, TypRuchu typRuchu)
+        {
+            lambda = lam;
+            typ = typRuchu;
+        }
+
+        
+        private TypRuchu typ;
+        private double lambda;
+        private double czasDoWyslania;
+        public TypRuchu getTypRuchu() { return typ; }
+
+        public double getCzasDoWyslania()
+        {
+            return czasDoWyslania;
+        }
+
+        public void zmniejszCzas(double czas)
+        {
+            czasDoWyslania -= czas;
+        }
+
+        public void losujCzas() // metoda odwracania dystrybuanty
+        {
+            var rand = new Random();
+            double y = rand.NextDouble();
+
+            double czas = Math.Log(1 - y) / (-lambda);
+            czasDoWyslania = czas;
+            Thread.Sleep(1);
+        }
+
+        public Pakiet wyslijPakiet()
+        {
+            losujCzas();
+            return new Pakiet(typ);
+        }
+    }
+}
